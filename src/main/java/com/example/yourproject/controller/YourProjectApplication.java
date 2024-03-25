403297c594084c796b66bce8178f69b8b8cf9b36
navyashree47 <mavyashree47@gmail.com>
2024-03-25 12:47:18 +0100

    adding script to remote

---
+++ src/main/java/com/example/yourproject/controller/YourProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.yourproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourProjectApplication.class, args);
+    }
+
+}
+
